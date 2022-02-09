c2a8928e1569664c00a4a9c12f27205961e48f03
lowit <ea@lowit.ru>
2022-02-10 00:12:17 +0300

    path_predicate: add path predicate

--- hitboxd-endpoint/src/path.rs
+++ hitboxd-endpoint/src/path.rs
@@ -1,20 +1,29 @@
 use crate::predicate::Predicate;
 use http::Request;
+use regex::Regex;
 
 #[derive(Debug)]
 pub(crate) struct Path {
-    inner: String,
+    exp: Regex,
 }
 
 impl Path {
-    pub(crate) fn new(inner: String) -> Self {
-        Self { inner }
+    pub(crate) fn new(tpl: String) -> Self {
+        let mut tpl = tpl
+            .replace("{digit}", r"\d+")
+            .replace("{string}", r"\D+")
+            .replace("{any}", r".+");
+        tpl = format!("^{}?", tpl);
+
+        Self {
+            exp: Regex::new(&tpl).unwrap(),
+        }
     }
 }
 
 impl<T> Predicate<Request<T>> for Path {
     fn predicate(&self, source: &Request<T>) -> bool {
-        self.inner == source.uri().path()
+        self.exp.is_match(source.uri().path())
     }
 }
 
@@ -26,11 +35,25 @@ mod tests {
 
     #[test]
     fn test_request_path() {
-        let request = Request::builder()
-            .uri("https://example.com/path/to/resource/")
-            .body(())
-            .unwrap();
-        let path = Path::new(String::from("/path/to/resource/"));
-        assert!(path.predicate(&request));
+        let cases = vec![
+            ("/path/to/resource/", "/path/to/resource/", true),
+            ("/resource/", "/path/to/resource/", false),
+            ("/path/to/resource/", "/path/{string}/resource/", true),
+            ("/res/42", "/res/{digit}", true),
+            ("/res/42/res2/name", "/res/{digit}/res2/{string}", true),
+            ("/res/42/res/name", "/res/{digit}/res2/{string}", false),
+            ("/res/42/res2/42", "/res/{digit}/res2/{string}", false),
+            ("/res/name/res2/name", "/res/{digit}/res2/{string}", false),
+            ("/res/42/res2/name", "/res/{any}", true),
+            ("/res/42/res2/name", r"/res/\d+/res\d/\w+", true),
+            ("/res/42/res/name", r"/res/\d+/res\d/\w+", false),
+        ];
+
+        for (path, exp, result) in cases {
+            let request = Request::builder().uri(path).body(()).unwrap();
+            let path = Path::new(exp.to_string());
+
+            assert!(path.predicate(&request) == result);
+        }
     }
 }
