0c3fa4ab5f46026491dc8c22c0851ae83fc3a799
lowit <ea@lowit.ru>
2022-02-23 01:21:58 +0300

    path_predicate: add actix-router

--- hitboxd-endpoint/src/path.rs
+++ hitboxd-endpoint/src/path.rs
@@ -1,29 +1,39 @@
 use crate::predicate::Predicate;
+use actix_router::ResourceDef;
 use http::Request;
 use regex::Regex;
 
 #[derive(Debug)]
+enum Pattern {
+    Regex(Regex),
+    Actix(ResourceDef),
+}
+
+#[derive(Debug)]
 pub(crate) struct Path {
-    exp: Regex,
+    pattern: Pattern,
 }
 
 impl Path {
-    pub(crate) fn new(tpl: String) -> Self {
-        let mut tpl = tpl
-            .replace("{digit}", r"\d+")
-            .replace("{string}", r"\D+")
-            .replace("{any}", r".+");
-        tpl = format!("^{}?", tpl);
+    pub(crate) fn new(path: String) -> Self {
+        let pattern = if path.starts_with("^") {
+            Pattern::Regex(Regex::new(&path).unwrap())
+        } else {
+            Pattern::Actix(ResourceDef::new(path))
+        };
 
-        Self {
-            exp: Regex::new(&tpl).unwrap(),
-        }
+        Self { pattern }
     }
 }
 
 impl<T> Predicate<Request<T>> for Path {
     fn predicate(&self, source: &Request<T>) -> bool {
-        self.exp.is_match(source.uri().path())
+        let path = source.uri().path();
+
+        match &self.pattern {
+            Pattern::Regex(p) => p.is_match(path),
+            Pattern::Actix(p) => p.is_match(path),
+        }
     }
 }
 
@@ -38,18 +48,18 @@ mod tests {
         let cases = vec![
             ("/path/to/resource/", "/path/to/resource/", true),
             ("/resource/", "/path/to/resource/", false),
-            ("/path/to/resource/", "/path/{string}/resource/", true),
-            ("/res/42", "/res/{digit}", true),
-            ("/res/42/res2/name", "/res/{digit}/res2/{string}", true),
-            ("/res/42/res/name", "/res/{digit}/res2/{string}", false),
-            ("/res/42/res2/42", "/res/{digit}/res2/{string}", false),
-            ("/res/name/res2/name", "/res/{digit}/res2/{string}", false),
-            ("/res/42/res2/name", "/res/{any}", true),
-            ("/res/42/res2/name", r"/res/\d+/res\d/\w+", true),
-            ("/res/42/res/name", r"/res/\d+/res\d/\w+", false),
+            ("/path/to/resource/", "/path/{arg1}/resource/", true),
+            ("/res/42", "/res/{arg1}", true),
+            ("/res/42/res2/name", "/res/{arg1}/res2/{arg2}", true),
+            ("/res/42/res/name", "/res/{arg1}/res2/{arg2}", false),
+            ("/res/42/res2/name", "/res/{arg1}", false),
+            ("/res/42/res2/name", r"^/res/\d+/res\d/\w+?", true),
+            ("/res/42/res/name", r"^/res/\d+/res\d/\w+?", false),
         ];
 
         for (path, exp, result) in cases {
+            dbg!(path, exp, result);
+
             let request = Request::builder().uri(path).body(()).unwrap();
             let path = Path::new(exp.to_string());
 
